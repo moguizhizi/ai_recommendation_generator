57118fb9c46b96bcf942863b06f67f11b797b7dd
devil-son <moguizhiz@126.com>
2026-03-03 08:10:24 +0000

    llm

--- app/controllers/chat_controller.py
+++ app/controllers/chat_controller.py
@@ -1,11 +1,19 @@
 # app/controllers/chat_controller.py
-from fastapi import APIRouter
+
+from fastapi import APIRouter, Request
 from app.schemas.chat import AIRecPlanRequest, AIRecPlanResponse
-from app.services.chat_service import  generate_ai_plan
+from app.services.chat_service import generate_ai_plan
+from llm.base import BaseLLM
 
 router = APIRouter()
 
+
 @router.post("/chat", response_model=AIRecPlanResponse)
-def chat_api(req: AIRecPlanRequest):
-    result = generate_ai_plan(req)
+def chat_api(req: AIRecPlanRequest, request: Request):
+    # 从 app.state 里拿到全局 LLM
+    llm: BaseLLM = request.app.state.llm
+
+    # 传给业务层
+    result = generate_ai_plan(req, llm)
+
     return AIRecPlanResponse(result=result)
