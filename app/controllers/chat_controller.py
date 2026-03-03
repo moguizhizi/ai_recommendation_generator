5f46ce36bb2c88df8998320ec4d03794093621f9
devil-son <moguizhiz@126.com>
2026-03-03 08:48:41 +0000

    chat_api log

--- app/controllers/chat_controller.py
+++ app/controllers/chat_controller.py
@@ -1,5 +1,7 @@
 # app/controllers/chat_controller.py
 
+import time
+import logging
 from fastapi import APIRouter, Request
 from app.schemas.chat import AIRecPlanRequest, AIRecPlanResponse
 from app.services.chat_service import generate_ai_plan
@@ -7,13 +9,38 @@ from llm.base import BaseLLM
 
 router = APIRouter()
 
+logger = logging.getLogger(__name__)
+
 
 @router.post("/chat", response_model=AIRecPlanResponse)
 def chat_api(req: AIRecPlanRequest, request: Request):
-    # 从 app.state 里拿到全局 LLM
+
     llm: BaseLLM = request.app.state.llm
 
-    # 传给业务层
-    result = generate_ai_plan(req, llm)
+    start_time = time.time()
 
-    return result
+    logger.info(
+        f"[CHAT_API_START] user_id={req.user_id} " f"patient_code={req.patient_code} "
+    )
+
+    try:
+        result = generate_ai_plan(req, llm)
+
+        duration = round(time.time() - start_time, 3)
+
+        logger.info(
+            f"[CHAT_API_SUCCESS] user_id={req.user_id} " f"duration={duration}s"
+        )
+
+        return result
+
+    except Exception as e:
+        duration = round(time.time() - start_time, 3)
+
+        logger.exception(
+            f"[CHAT_API_ERROR] user_id={req.user_id} "
+            f"duration={duration}s "
+            f"error={str(e)}"
+        )
+
+        raise
